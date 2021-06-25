ab75d9a8dae0a6a5c63a9b40f5fe39e8ccdccea2
Aldayr Ruiz <aldayr.ruiz@opendeusto.es>
2021-06-25 19:53:02 +0200

    feat: Add gps map

--- src/app/pages/gps-positions/gps-positions.component.ts
+++ src/app/pages/gps-positions/gps-positions.component.ts
@@ -1,12 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-gps-positions',
   templateUrl: './gps-positions.component.html',
   styleUrls: ['./gps-positions.component.css'],
 })
-export class GpsPositionsComponent implements OnInit {
+export class GpsPositionsComponent implements AfterViewInit {
+
+  private mapHtmlId = 'map';
+  private map: L.Map;
+  
   constructor() {}
 
-  ngOnInit(): void {}
+  initMap(): void {
+    const mapPosition: [number, number] = [40.423516, -4.202832] // Madrid, Spain
+    const initialZoom = 6;
+    
+    const attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+    const urlTemplate = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    const maxZoom = 18;
+    const minZoom = 3;
+
+    this.map = L.map(this.mapHtmlId).setView(mapPosition, initialZoom);
+
+    const tiles = L.tileLayer(urlTemplate, {
+      maxZoom: maxZoom,
+      minZoom: minZoom,
+      attribution: attribution
+    });
+
+    tiles.addTo(this.map);
+  }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
 }
