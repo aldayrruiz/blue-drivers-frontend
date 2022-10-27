9172898ce02fbef06b53e2b858d3023e3da4a306
Aldayr Ruiz <45878892+aldayrruiz@users.noreply.github.com>
2022-10-27 11:28:27 +0200

    feat: Change fuel prices & justify text stats

--- src/app/core/services/measure/fuel-price/diesel.calculator.service.ts
+++ src/app/core/services/measure/fuel-price/diesel.calculator.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@angular/core';
 import { PriceFuelCalculator } from './calculator.interface';
 
-// * Si diésel: 6 litros/100km a 1.5€/litro de diésel
 @Injectable({ providedIn: 'root' })
 export class DieselPriceFuelCalculator implements PriceFuelCalculator {
   getPrice(distance: number): number {
-    // * 9€ is equivalent to drive during 100km
-    const price = (distance * 9) / 100;
+    // * 1,8 euros/litro y 8 litros/100km
+    const price = (distance * 1.8 * 8) / 100;
     return price;
   }
 }
