624ccf6ffa710738cd3b16bc25c0a987a717cf51
Aldayr Ruiz <aldayr2000@gmail.com>
2023-06-09 16:54:00 +0200

    feat: Big icons

--- src/app/modules/map/components/vehicles/vehicles.component.ts
+++ src/app/modules/map/components/vehicles/vehicles.component.ts
@@ -186,7 +186,7 @@ export class VehiclesComponent implements OnInit, AfterViewInit {
   private createLeafIcon(iconSrc: string) {
     return L.icon({
       iconUrl: iconSrc,
-      iconSize: [22, 22], // size of the icon
+      iconSize: [35, 35], // size of the icon
       iconAnchor: [10, 10], // point of the icon which will correspond to marker's location
       popupAnchor: [0, 0], // point from which the popup should open relative to the iconAnchor
       className: 'leaflet-vehicle-icon',
